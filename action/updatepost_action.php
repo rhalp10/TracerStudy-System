2fb858c76591aa9f399ccce6ac58e4de23323007
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-10-12 00:31:40 +0800

    change windows location

--- action/updatepost_action.php
+++ action/updatepost_action.php
@@ -1,5 +1,6 @@
 <?php 
 $con = mysqli_connect('localhost','root','','tracerdata') or die("ERROR");
+
 $post_ID = $_REQUEST['post_ID'];
 if (isset($_POST['submit_updatetopic'])) {
 	$updatepost_title = $_POST['updatepost_title'];
@@ -12,7 +13,7 @@ if (isset($_POST['submit_updatetopic'])) {
 	mysqli_query($con,"UPDATE `forum_topic` SET  `post_title` = '$updatepost_title' ,`post_content` = '$updatepost_content', `post_status` = 'UNPIN' WHERE `topic_ID` = '$post_ID'");
 $req_encypted_postID = password_hash($post_ID, PASSWORD_DEFAULT);
 	echo "<script>alert('Successfully Updated');
-											window.location='../forum_view.php?post_ID=$req_encypted_postID';
+											window.location='../profile_post.php';
 										</script>";
 
 }
