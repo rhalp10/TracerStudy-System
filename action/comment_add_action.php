e8deb71dc9424c36460869a1b3a70f704d98a376
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-10-16 20:38:41 +0800

    comment add action fix

--- action/comment_add_action.php
+++ action/comment_add_action.php
@@ -2,7 +2,7 @@
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $con = mysqli_connect('localhost','root','','tracerdata') or die("ERROR");
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit-comment'])) {
 	$comment = $_POST['comment'];
 	$owner = $_REQUEST['userID_comment'];
 	$topicID = $_REQUEST['comment_topicID'];
@@ -13,10 +13,32 @@ if (isset($_POST['submit'])) {
 	$comment = mysqli_real_escape_string($con,$comment);
 	$owner = mysqli_real_escape_string($con,$owner);
 	$topicID = mysqli_real_escape_string($con,$topicID);
-	$result = mysqli_query($con,"INSERT INTO `forum_comment` (`comment_ID`, `comment_topicID`, `comment_userID`, `comment_content`, `comment_date`) VALUES (NULL, '$topicID', '$owner', '$comment', CURRENT_TIMESTAMP)");
+	// requested post data id
+	$req_encypted_postID = $topicID;
+	/*FOR VERIFYING topic requested HASHED ID*/
+	// selecting all data from database
+	$query_verify_id = mysqli_query($con,"SELECT * FROM `forum_topic`");
+	//while fetching all data
+	while ($res_ver_id = mysqli_fetch_array($query_verify_id)) 
+	{
+	  //each topic id mush be save in temp variable of check_id
+	  $check_id = $res_ver_id['topic_ID'];
+	    //the requested hash checked original value if match then stored the verified value in verified_id
+	    if (password_verify($check_id, $req_encypted_postID)) 
+	      {
+	      $verified_id = $check_id;//temporary value save to verified_id
+	      }  
+	      
+	}
+
+
+	
+	$sql="INSERT INTO `forum_comment` (comment_ID, comment_topicID, comment_userID, comment_content, comment_date)";
+	$sql.=" VALUES (NULL, '$verified_id', '$owner', '$comment', CURRENT_TIMESTAMP)";
+	$result = mysqli_query($con,$sql);
 
 	echo "<script>alert('Successfully Comment');
-						window.location='../index.php';
+						window.location='../forum_view.php?post_ID=$topicID';
 					</script>";
 
 	
