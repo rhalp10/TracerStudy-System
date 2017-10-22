bad1e181820eaaf982dce2091c29aad64cbdcb4c
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-10-22 20:03:19 +0800

    post status

--- action/post_status.php
+++ action/post_status.php
@@ -3,7 +3,15 @@
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $con = mysqli_connect('localhost','root','','tracerdata') or die("ERROR");
 $stat = $_REQUEST['stat'];
+$pID = $_REQUEST['pID'];
 $stat = stripslashes($stat);
+$pID = stripslashes($pID);
 $stat = mysqli_real_escape_string($con,$stat);
+$pID = mysqli_real_escape_string($con,$pID);
+$hashID = password_hash($pID, PASSWORD_DEFAULT);
+mysqli_query($con,"UPDATE `forum_topic` SET `post_status` = '$stat' WHERE `topic_ID` = '$pID'");
+echo "<script>alert('Successfully $stat');
+											window.location='../forum_view.php?post_ID=$hashID';
+										</script>";
 
 ?>
