2c57cf41a31c398ff827d069fad54ae829d77c80
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-10-26 00:42:24 +0800

    update using formatter

--- script.php
+++ script.php
@@ -1,111 +1,107 @@
- <!--jQuery -->
-            <script src="assets/lib/jquery/jquery.js"></script>
+<!--jQuery -->
+<script src="assets/lib/jquery/jquery.js"></script>
 
-                    <script src="//cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script>
 
-            <!--Bootstrap -->
-            <script src="assets/lib/bootstrap/js/bootstrap.js"></script>
-            <!-- MetisMenu -->
-            <script src="assets/lib/metismenu/metisMenu.js"></script>
-            <!-- onoffcanvas -->
-            <script src="assets/lib/onoffcanvas/onoffcanvas.js"></script>
-            <!-- Screenfull -->
-            <script src="assets/lib/screenfull/screenfull.js"></script>
+<!--Bootstrap -->
+<script src="assets/lib/bootstrap/js/bootstrap.js"></script>
+<!-- MetisMenu -->
+<script src="assets/lib/metismenu/metisMenu.js"></script>
+<!-- onoffcanvas -->
+<script src="assets/lib/onoffcanvas/onoffcanvas.js"></script>
+<!-- Screenfull -->
+<script src="assets/lib/screenfull/screenfull.js"></script>
 
-
-            <!-- Metis core scripts -->
-            <script src="assets/js/core.js"></script>
-            <!-- Metis demo scripts 
-
+<!-- Metis core scripts -->
+<script src="assets/js/core.js"></script>
+<!-- Metis demo scripts 
 
             <script src="assets/js/style-switcher.js"></script>-->
-            <script src="assets/js/app.js"></script>
+<script src="assets/js/app.js"></script>
 
-     <script type="text/javascript" language="javascript" src="assets/js/jquery.js"></script>
-    <script type="text/javascript" language="javascript" src="assets/js/jquery.dataTables.js"></script>
+<script type="text/javascript" language="javascript" src="assets/js/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="assets/js/jquery.dataTables.js"></script>
 
-    <script src="assets/js/jquery.dataTables.min.js"></script>
-    <script src="assets/js/dataTables.bootstrap.min.js"></script>
+<script src="assets/js/jquery.dataTables.min.js"></script>
+<script src="assets/js/dataTables.bootstrap.min.js"></script>
 
-  
-    <script type="text/javascript">
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#myData').DataTable({
+            //"paging":   false,
+            "ordering": false,
+            "info": false,
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "bAutoWidth": false
+        });
+    });
+    $(document).ready(function() {
+        $('#myData1').DataTable({
+            //"paging":   false,
+            "ordering": false,
+            "info": false,
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "bAutoWidth": false
+        });
+    });
 
-      $(document).ready(function() {
-    $('#myData').DataTable( {
-        //"paging":   false,
-        "ordering": false,
-        "info":     false,
-        "bLengthChange": false,
-        "bFilter": true,
-        "bInfo": false,
-        "bAutoWidth": false 
-    } );
-} );
-        $(document).ready(function() {
-    $('#myData1').DataTable( {
-        //"paging":   false,
-        "ordering": false,
-        "info":     false,
-        "bLengthChange": false,
-        "bFilter": true,
-        "bInfo": false,
-        "bAutoWidth": false 
-    } );
-} );
+    $(document).ready(function() {
+        $('#alumniIT').DataTable({
+            //"paging":   false,
+            "ordering": false,
+            "info": false,
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "bAutoWidth": false
+        });
+    });
 
-      $(document).ready(function() {
-    $('#alumniIT').DataTable( {
-        //"paging":   false,
-        "ordering": false,
-        "info":     false,
-        "bLengthChange": false,
-        "bFilter": true,
-        "bInfo": false,
-        "bAutoWidth": false 
-    } );
-} );
+    $(document).ready(function() {
+        $('#alumniCS').DataTable({
+            //"paging":   false,
+            "ordering": false,
+            "info": false,
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "bAutoWidth": false
+        });
+    });
+    $(document).ready(function() {
+        $('#alumniOA').DataTable({
+            //"paging":   false,
+            "ordering": false,
+            "info": false,
+            "bLengthChange": false,
+            "bFilter": true,
+            "bInfo": false,
+            "bAutoWidth": false
+        });
+    });
 
-      $(document).ready(function() {
-    $('#alumniCS').DataTable( {
-        //"paging":   false,
-        "ordering": false,
-        "info":     false,
-        "bLengthChange": false,
-        "bFilter": true,
-        "bInfo": false,
-        "bAutoWidth": false 
-    } );
-} );
-       $(document).ready(function() {
-    $('#alumniOA').DataTable( {
-        //"paging":   false,
-        "ordering": false,
-        "info":     false,
-        "bLengthChange": false,
-        "bFilter": true,
-        "bInfo": false,
-        "bAutoWidth": false 
-    } );
-} );
-      
-   //NUMBER ONLY
-  function numberInputOnly(elem) {
-                var validChars = /[0-9]/;
-                var strIn = elem.value;
-                var strOut = '';
-                for(var i=0; i < strIn.length; i++) {
-                  strOut += (validChars.test(strIn.charAt(i)))? strIn.charAt(i) : '';
-                }
-                elem.value = strOut;
-            }
-  //LETTER ONLY
-   function letterInputOnly(elem) {
-                var validChars = /[a-zA-ZñÑ .]+/;
-                var strIn = elem.value;
-                var strOut = '';
-                for(var i=0; i < strIn.length; i++) {
-                  strOut += (validChars.test(strIn.charAt(i)))? strIn.charAt(i) : '';
-                }
-                elem.value = strOut;
-            }
-    </script>
+    //NUMBER ONLY
+    function numberInputOnly(elem) {
+        var validChars = /[0-9]/;
+        var strIn = elem.value;
+        var strOut = '';
+        for (var i = 0; i < strIn.length; i++) {
+            strOut += (validChars.test(strIn.charAt(i))) ? strIn.charAt(i) : '';
+        }
+        elem.value = strOut;
+    }
+    //LETTER ONLY
+    function letterInputOnly(elem) {
+        var validChars = /[a-zA-ZñÑ .]+/;
+        var strIn = elem.value;
+        var strOut = '';
+        for (var i = 0; i < strIn.length; i++) {
+            strOut += (validChars.test(strIn.charAt(i))) ? strIn.charAt(i) : '';
+        }
+        elem.value = strOut;
+    }
+</script>
--- style_css.php
+++ style_css.php
@@ -1,42 +1,38 @@
+<!-- Bootstrap -->
+<link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.css">
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.css">
-    
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="assets/lib/font-awesome/css/font-awesome.css">
-    
-    <!-- Metis core stylesheet -->
-    <link rel="stylesheet" href="assets/css/main.css">
-    
-    <!-- metisMenu stylesheet -->
-    <link rel="stylesheet" href="assets/lib/metismenu/metisMenu.css">
-    
-    <!-- onoffcanvas stylesheet -->
-    <link rel="stylesheet" href="assets/lib/onoffcanvas/onoffcanvas.css">
-    
-    <!-- animate.css stylesheet -->
-    <link rel="stylesheet" href="assets/lib/animate.css/animate.css">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="assets/lib/font-awesome/css/font-awesome.css">
 
-     <!-- ckeditor css stylesheet -->
-    <script src="assets/lib/ckeditor/ckeditor.js"></script>
+<!-- Metis core stylesheet -->
+<link rel="stylesheet" href="assets/css/main.css">
 
+<!-- metisMenu stylesheet -->
+<link rel="stylesheet" href="assets/lib/metismenu/metisMenu.css">
 
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.css">
-     <!-- dataTable css stylesheet -->
-    <link href="assets/css/dataTables.bootstrap.min.css" rel="stylesheet">
-        <style type="text/css">
-      .dataTables_filter{
+<!-- onoffcanvas stylesheet -->
+<link rel="stylesheet" href="assets/lib/onoffcanvas/onoffcanvas.css">
+
+<!-- animate.css stylesheet -->
+<link rel="stylesheet" href="assets/lib/animate.css/animate.css">
+
+<!-- ckeditor css stylesheet -->
+<script src="assets/lib/ckeditor/ckeditor.js"></script>
+
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.css">
+<!-- dataTable css stylesheet -->
+<link href="assets/css/dataTables.bootstrap.min.css" rel="stylesheet">
+<style type="text/css">
+    .dataTables_filter {
         font-family: lato;
-        
-      }
-      .dataTables_filter > label > input{
+    }
     
-    padding: 5px 20px;
-    margin: 8px 0;
-    box-sizing: border-box;
-
-      }
-      </style>
+    .dataTables_filter > label > input {
+        padding: 5px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+    }
+</style>
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -45,7 +41,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 
-    <!--For Development Only. Not required 
+<!--For Development Only. Not required 
     <script>
         less = {
             env: "development",
@@ -54,15 +50,12 @@
         };
     </script>
     <link rel="stylesheet" href="assets/css/style-switcher.css">-->
-    
-    <link rel="stylesheet/less" type="text/css" href="assets/less/theme.less">
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.js"></script>
-    <script src="assets/lib/chart-master/dist/Chart.bundle.js"></script>
-    <script src="assets/lib/chart-master/samples/utils.js"></script>
 
+<link rel="stylesheet/less" type="text/css" href="assets/less/theme.less">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.js"></script>
+<script src="assets/lib/chart-master/dist/Chart.bundle.js"></script>
+<script src="assets/lib/chart-master/samples/utils.js"></script>
 
 <script src="sweetalert-master/sweetalert.min.js"></script>
 <link rel="stylesheet" type="text/css" href="sweetalert-master/sweetalert.css">
-
- 
