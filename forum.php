602a02b4c85dea9b07135bfbb239f22196b8ecdd
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-10-28 02:53:30 +0800

    forum view table update

--- forum.php
+++ forum.php
@@ -58,7 +58,7 @@ else {}
                             <!-- /.main-bar -->
                         </header>
                         <div class="inner bg-light lter">
-                           <table id="myData1"  class="table table-bordered table-advance table-hover  dataTable">
+                           <table id="forumData_Pin"  class="table table-bordered table-advance table-hover  dataTable">
                                   <thead>
                                     <tr>
                                       <th><h3>Pinned Topics</h3></th>
@@ -69,64 +69,9 @@ else {}
                                       <th></th>
                                     </tr>
                                   </tfoot>
-                                  <tbody>
-                                  <?php 
-                                  // SELECT * FROM `forum_topic` ORDER BY `forum_topic`.`post_date` DESC
-                                  $query = mysqli_query($con,"SELECT * FROM `forum_topic` WHERE `post_status` = 'PIN' ORDER BY `post_date` ASC;");
-                                  while($res = mysqli_fetch_array($query)) {
-                                     $query2 = mysqli_query($con,"SELECT `view_count` FROM `view_counter` WHERE `view_topicID` = ".$res['topic_ID']);
-                                     $res2 = mysqli_fetch_assoc($query2);
-                                  
-                                  ?>
-                                  <tr onclick="self.location.href='forum_view.php?post_ID=<?php echo password_hash($res['topic_ID'], PASSWORD_DEFAULT);?>'">
-                                  <td class="forum-td" >
-                                  <div class="forum-list-hover col-sm-1" >
-                                  <i class="fa fa-comment"></i>
-                                    </div>
-                                    <div class="col-sm-6 forum-list-content">
-                                   <strong><a href="forum_view.php?post_ID=<?php echo password_hash($res['topic_ID'], PASSWORD_DEFAULT);?>"><?php echo $res['post_title']; ?>
-                                    </a></strong>
-                                    <br>
-                                    by <a href=""><?php 
-                                    $post_owner = $res['post_owner_id'];
-                                    if($query_postowner = mysqli_query($con,"SELECT student_fName,student_mName,student_lName FROM `user_student_detail` WHERE `student_userID` = '$post_owner'")) {
-                                       $res_postowner = mysqli_fetch_assoc($query_postowner);
-                                       echo $res_postowner['student_fName'].
-                                       " ".$res_postowner['student_mName'].
-                                       " ".$res_postowner['student_lName'];
-                                    }
-                                    if($query_postowner = mysqli_query($con,"SELECT teacher_fName,teacher_mName,teacher_lName FROM `user_teacher_detail` WHERE `teacher_userID` = '$post_owner'")) {
-                                       $res_postowner = mysqli_fetch_assoc($query_postowner);
-                                       echo $res_postowner['teacher_fName'].
-                                       " ".$res_postowner['teacher_mName'].
-                                       " ".$res_postowner['teacher_lName'];
-                                    }
-                                    if($query_postowner = mysqli_query($con,"SELECT admin_fName,admin_mName,admin_lName FROM `user_admin_detail` WHERE `admin_userID` = '$post_owner'")) {
-                                       $res_postowner = mysqli_fetch_assoc($query_postowner);
-                                       echo $res_postowner['admin_fName'].
-                                       " ".$res_postowner['admin_mName'].
-                                       " ".$res_postowner['admin_lName'];
-                                    } ?></a>
-                                    </div>
-                                    <div class="col-sm-2 forum-list-content-stat">
-                                    <?php echo $res2['view_count'] ;?> <i class="fa fa-eye"></i>
-                                    <br>
-                                    2 <i class="fa fa-comment"></i>
-                                    </div>
-                                    <div class="col-sm-3" style="background-color: #444444;color: white;"><br>&nbsp;
-                                    </div>
-
-                                    </td>
-                                  </tr>
-                                  <?php 
-                                  }?>
-                                  </tbody>
-                                  <script type="text/javascript">
-                                    
-                                  </script>
                               </table>
                               <hr>
-                              <table id="myData"  class="table table-bordered table-advance table-hover  dataTable">
+                              <table id="forumData_Unpin"  class="table table-bordered table-advance table-hover  dataTable">
                                   <thead>
                                     <tr>
                                       <th><h3>Topic</h3></th>
@@ -157,60 +102,7 @@ else {}
                                       <th></th>
                                     </tr>
                                   </tfoot>
-                                  <tbody>
-                                  
-                                  <?php 
-                                  $query3 = mysqli_query($con,"SELECT * FROM `forum_topic` WHERE `post_status` = 'UNPIN'  ORDER BY `post_date` DESC");
-                                  while($res3 = mysqli_fetch_array($query3)) {
-                                     $query4 = mysqli_query($con,"SELECT `view_count` FROM `view_counter` WHERE `view_topicID` = ".$res3['topic_ID']);
-                                     $res4 = mysqli_fetch_assoc($query4);
-                                  ?>
-                                  <tr onclick="self.location.href='forum_view.php?post_ID=<?php echo password_hash($res3['topic_ID'], PASSWORD_DEFAULT);?>'">
-                                  <td class="forum-td" >
-                                  <div class="forum-list-hover col-sm-1" >
-                                  <i class="fa fa-comment"></i>
-                                    </div>
-                                    <div class="col-sm-6 forum-list-content">
-                                   <strong><a href="forum_view.php?post_ID=<?php echo password_hash($res3['topic_ID'], PASSWORD_DEFAULT);?>"><?php echo $res3['post_title']; ?>
-                                    </a></strong>
-                                    <br>
-                                    by <a href="profile.php"><?php 
-                                    $post_owner = $res3['post_owner_id'];
-                                    if($query_postowner = mysqli_query($con,"SELECT student_fName,student_mName,student_lName FROM `user_student_detail` WHERE `student_userID` = '$post_owner'")) {
-                                       $res_postowner = mysqli_fetch_assoc($query_postowner);
-                                       echo $res_postowner['student_fName'].
-                                       " ".$res_postowner['student_mName'].
-                                       " ".$res_postowner['student_lName'];
-                                    }
-                                    if($query_postowner = mysqli_query($con,"SELECT teacher_fName,teacher_mName,teacher_lName FROM `user_teacher_detail` WHERE `teacher_userID` = '$post_owner'")) {
-                                       $res_postowner = mysqli_fetch_assoc($query_postowner);
-                                       echo $res_postowner['teacher_fName'].
-                                       " ".$res_postowner['teacher_mName'].
-                                       " ".$res_postowner['teacher_lName'];
-                                    }
-                                    if($query_postowner = mysqli_query($con,"SELECT admin_fName,admin_mName,admin_lName FROM `user_admin_detail` WHERE `admin_userID` = '$post_owner'")) {
-                                       $res_postowner = mysqli_fetch_assoc($query_postowner);
-                                       echo $res_postowner['admin_fName'].
-                                       " ".$res_postowner['admin_mName'].
-                                       " ".$res_postowner['admin_lName'];
-                                    }?></a>
-                                    </div>
-                                    <div class="col-sm-2 forum-list-content-stat">
-                                    <?php echo $res4['view_count'] ;?> <i class="fa fa-eye"></i>
-                                    <br>
-                                    2 <i class="fa fa-comment"></i>
-                                    </div>
-                                    <div class="col-sm-3" style="background-color: #444444;color: white;">
-                                   &nbsp;<br>&nbsp;
-                                    </div>
-
-                                    </td>
-                                  </tr>
-                                  <?php 
-                                  }
-
-                                  ?>
-                                  </tbody>
+                     
                                   <script type="text/javascript">
                                     
                                   </script>
