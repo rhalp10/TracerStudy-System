1cc0430d8a7d7b30f4a72e52c554ca449cc2c9e2
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-10-27 22:43:22 +0800

    move to script.php

--- profile.php
+++ profile.php
@@ -669,29 +669,3 @@ else
 </html>
 
 
-<script type="text/javascript">
-$('.btn[data-toggle=modal]').on('click', function(){
-  var $btn = $(this);
-  var currentDialog = $btn.closest('.modal-dialog'),
-  targetDialog = $($btn.attr('data-target'));;
-  if (!currentDialog.length)
-    return;
-  targetDialog.data('previous-dialog', currentDialog);
-  currentDialog.addClass('aside');
-  var stackedDialogCount = $('.modal.in .modal-dialog.aside').length;
-  if (stackedDialogCount <= 5){
-    currentDialog.addClass('aside-' + stackedDialogCount);
-  }
-});
-
-$('.modal').on('hide.bs.modal', function(){
-  var $dialog = $(this);  
-  var previousDialog = $dialog.data('previous-dialog');
-  if (previousDialog){
-    previousDialog.removeClass('aside');
-    $dialog.data('previous-dialog', undefined);
-  }
-});
-
-
-</script>
