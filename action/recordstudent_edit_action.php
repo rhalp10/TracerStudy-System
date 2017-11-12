fa7fc354668555eee3bfb1db52df65590c1088b9
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-11-12 14:56:17 +0800

    record student edit action update

--- action/recordstudent_edit_action.php
+++ action/recordstudent_edit_action.php
@@ -1,5 +1,88 @@
 <?php 
-// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$con = mysqli_connect('localhost','root','','tracerdata') or die("ERROR");
+	$con = mysqli_connect('localhost','root','','tracerdata') or die("ERROR");
+	if (isset($_POST['Submit'])) {
+		$studentID = $_REQUEST['studentID'];
+		// Defining post variable names 
+		$student_sinumber = $_POST['student_sinumber'];
+		$student_firstname = $_POST['student_firstname'];
+		$student_middlename = $_POST['student_middlename'];
+		$student_lastname = $_POST['student_lastname'];
+		$student_adress = $_POST['student_adress'];
+		$student_year_grad = $_POST['student_year_grad'];
+		$student_year_admission = $_POST['student_year_admission'];
+		$student_department = $_POST['student_department'];
+		// To protect MySQL injection for Security purpose
+		$student_sinumber = stripslashes($student_sinumber);
+		$student_firstname = stripslashes($student_firstname);
+		$student_middlename = stripslashes($student_middlename);
+		$student_lastname = stripslashes($student_lastname);
+		$student_adress = stripslashes($student_adress);
+		$student_year_grad = stripslashes($student_year_grad);
+		$student_year_admission = stripslashes($student_year_admission);
+		$student_department = stripslashes($student_department);
 
+		$student_sinumber = mysqli_real_escape_string($con,$student_sinumber);
+		$student_firstname = mysqli_real_escape_string($con,$student_firstname);
+		$student_middlename = mysqli_real_escape_string($con,$student_middlename);
+		$student_lastname = mysqli_real_escape_string($con,$student_lastname);
+		$student_adress = mysqli_real_escape_string($con,$student_adress);
+		$student_year_grad = mysqli_real_escape_string($con,$student_year_grad);
+		$student_year_admission = mysqli_real_escape_string($con,$student_year_admission);
+		$student_department = mysqli_real_escape_string($con,$student_department);
+
+if (empty($student_sinumber) || empty($student_firstname)|| empty($student_middlename)|| empty($student_lastname)|| empty($student_adress)|| empty($student_year_grad)|| empty($student_year_admission)|| empty($student_department)) {
+	if (empty($student_sinumber) ) {
+		echo "<script>alert('Empty student_sinumber !');
+												window.location='../recordstudent.php';
+											</script>";
+	}
+	if (empty($student_firstname)) {
+		echo "<script>alert('Empty student_firstname !');
+												window.location='../recordstudent.php';
+											</script>";
+	}
+	if (empty($student_middlename)) {
+		echo "<script>alert('Empty student_middlename !');
+												window.location='../recordstudent.php';
+											</script>";
+	}
+	if (empty($student_lastname)) {
+		echo "<script>alert('Empty student_lastname !');
+												window.location='../recordstudent.php';
+											</script>";
+	}
+	if (empty($student_adress)) {
+		echo "<script>alert('Empty student_adress !');
+												window.location='../recordstudent.php';
+											</script>";
+	}
+	if (empty($student_year_grad)) {
+		echo "<script>alert('Empty student_year_grad !');
+												window.location='../recordstudent.php';
+											</script>";
+	}
+	if (empty($student_year_admission)) {
+		echo "<script>alert('Empty student_year_admission !');
+												window.location='../recordstudent.php';
+											</script>";
+	}
+	if (empty($student_department)) {
+		echo "<script>alert('Empty student_department !');
+												window.location='../recordstudent.php';
+											</script>";
+	}
+}
+else{
+		//insert query
+		$sql = "UPDATE `user_student_detail` SET  student_IDNumber = '$student_sinumber', student_fName = '$student_firstname', student_mName = '$student_middlename', student_lName = '$student_lastname', student_address = '$student_adress', student_admission = '$student_year_admission', student_year_grad = '$student_year_grad', student_department = '$student_department'";
+
+		$sql.= "  WHERE `user_student_detail`.`student_ID` = $studentID;";
+		$res = mysqli_query($con,$sql);
+		
+		echo "<script>alert('Successfully Update!');
+												window.location='../recordstudent.php';
+											</script>";
+}
+	
+	}
 ?>
