27db656ab0e86fab88a06b88bf211bfcc2d74908
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-11-15 16:30:00 +0800

    recordstudentaction update

--- action/recordstudent_action.php
+++ action/recordstudent_action.php
@@ -7,6 +7,9 @@
 		$student_firstname = $_POST['student_firstname'];
 		$student_middlename = $_POST['student_middlename'];
 		$student_lastname = $_POST['student_lastname'];
+		$student_dob = $_POST['student_dob'];
+		$student_gender = $_POST['student_gender'];
+		$student_civilStat = $_POST['student_civilStat'];
 		$student_adress = $_POST['student_adress'];
 		$student_year_grad = $_POST['student_year_grad'];
 		$student_year_admission = $_POST['student_year_admission'];
@@ -16,15 +19,20 @@
 		$student_firstname = stripslashes($student_firstname);
 		$student_middlename = stripslashes($student_middlename);
 		$student_lastname = stripslashes($student_lastname);
+		$student_dob = stripslashes($student_dob);
+		$student_gender = stripslashes($student_gender);
+		$student_civilStat = stripslashes($student_civilStat);
 		$student_adress = stripslashes($student_adress);
 		$student_year_grad = stripslashes($student_year_grad);
 		$student_year_admission = stripslashes($student_year_admission);
 		$student_department = stripslashes($student_department);
-
 		$student_sinumber = mysqli_real_escape_string($con,$student_sinumber);
 		$student_firstname = mysqli_real_escape_string($con,$student_firstname);
 		$student_middlename = mysqli_real_escape_string($con,$student_middlename);
 		$student_lastname = mysqli_real_escape_string($con,$student_lastname);
+		$student_dob = mysqli_real_escape_string($con,$student_dob);
+		$student_gender = mysqli_real_escape_string($con,$student_gender);
+		$student_civilStat = mysqli_real_escape_string($con,$student_civilStat);
 		$student_adress = mysqli_real_escape_string($con,$student_adress);
 		$student_year_grad = mysqli_real_escape_string($con,$student_year_grad);
 		$student_year_admission = mysqli_real_escape_string($con,$student_year_admission);
@@ -74,8 +82,8 @@ if (empty($student_sinumber) || empty($student_firstname)|| empty($student_middl
 }
 else{
 		//insert query
-		$sql = "INSERT INTO `user_student_detail` (student_ID, student_userID, student_img, student_IDNumber, student_fName, student_mName, student_lName, student_address, student_admission, student_year_grad, student_department, student_status) ";
-		$sql.= " VALUES (NULL, '0', 'temp.gif', '$student_sinumber', '$student_firstname', '$student_middlename', '$student_lastname', '$student_adress', '$student_year_admission', '$student_year_grad', '$student_department', 'unregister')";
+		$sql = "INSERT INTO `user_student_detail` (student_ID, student_userID, student_img, student_IDNumber, student_fName, student_mName, student_lName, student_address, student_admission, student_year_grad, student_department, student_status, student_civilStat, student_dob, student_gender) ";
+		$sql.= " VALUES (NULL, '0', 'temp.gif', '$student_sinumber', '$student_firstname', '$student_middlename', '$student_lastname', '$student_adress', '$student_year_admission', '$student_year_grad', '$student_department', 'unregister', '$student_civilStat', '$student_dob', '$student_gender')";
 		$res = mysqli_query($con,$sql);
 		
 		echo "<script>alert('Successfully Added!');
