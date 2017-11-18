67c79c2f7070a65689d2a148ea2e24bba68caff4
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-11-18 20:47:49 +0800

    Script update

--- script.php
+++ script.php
@@ -337,6 +337,34 @@ $(document).ready(function() {
                 } );
               
             } );
+
+ $(document).ready(function() {
+    $('#example').DataTable( {
+        serverSide: true,
+        ordering: false,
+        searching: false,
+        ajax: function ( data, callback, settings ) {
+            var out = [];
+ 
+            for ( var i=data.start, ien=data.start+data.length ; i<ien ; i++ ) {
+                out.push( [ i+'-1', i+'-2', i+'-3', i+'-4', i+'-5' ] );
+            }
+ 
+            setTimeout( function () {
+                callback( {
+                    draw: data.draw,
+                    data: out,
+                    recordsTotal: 5000000,
+                    recordsFiltered: 5000000
+                } );
+            }, 50 );
+        },
+        scrollY: 200,
+        scroller: {
+            loadingIndicator: true
+        }
+    } );
+} );
     //NUMBER ONLY
     function numberInputOnly(elem) {
         var validChars = /[0-9]/;
