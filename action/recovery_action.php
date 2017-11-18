c7ece3b39de7fd8571b02f78ac5e6e68bc0d3b50
rhalp10 <rhalpdarrencabrera@gmail.com>
2017-11-19 05:05:25 +0800

    removed

--- action/recovery_action.php
+++
@@ -1,5 +0,0 @@
-<?php 
-// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$con = mysqli_connect('localhost','root','','tracerdata') or die("ERROR");
-
-?>
