d52b2c886406bcc49cf4fe422c5338b17c5bb928
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2018-01-21 02:23:00 +0800

    batchmate fix

--- batchmates.php
+++ batchmates.php
@@ -1,77 +1,74 @@
 
 <?php 
-include('session.php'); 
+include('session.php');
 include('db.php');
-
-$survey_maxcount_qry = mysqli_query($con,"SELECT survey_maxattemp FROM `survey_maxcount` WHERE survey_ownerID = '$login_id'");
-$survey_maxattemp = mysqli_fetch_array($survey_maxcount_qry);
-$page = 'batchmates';
-
+$page = 'alumni';
 if ($login_level == '1')
 {
-    $result = mysqli_query($con,"SELECT * FROM `user_student_detail` WHERE student_userID = $login_id");
-    $data = mysqli_fetch_array($result);
-    $data_img = $data['student_img']; 
+  $result = mysqli_query($con,"SELECT * FROM `user_student_detail` WHERE student_userID = $login_id");
+  $data = mysqli_fetch_array($result);
+  $data_img = $data['student_img'];
 }
 else if ($login_level == '2')
 {
-    $result = mysqli_query($con,"SELECT * FROM `user_teacher_detail` WHERE teacher_userID = $login_id");
-    $data = mysqli_fetch_array($result);
-    $data_img = $data['teacher_img']; 
+  $result = mysqli_query($con,"SELECT * FROM `user_teacher_detail` WHERE teacher_userID = $login_id");
+  $data = mysqli_fetch_array($result);
+  $data_img = $data['teacher_img'];
 }
 else if ($login_level == '3')
 {
-    $result = mysqli_query($con,"SELECT * FROM `user_admin_detail` WHERE admin_userID = $login_id");
-    $data = mysqli_fetch_array($result);
-    $data_img = $data['admin_img']; 
+  $result = mysqli_query($con,"SELECT * FROM `user_admin_detail` WHERE admin_userID = $login_id");
+  $data = mysqli_fetch_array($result);
+  $data_img = $data['admin_img'];
 }
 else
-{
-}
-?><!DOCTYPE html>
+{}
+
+?>
+<!DOCTYPE html>
 <html>  
   <head>
     <?php include('meta.php');?>
     <?php include('style_css.php');?>
-    <title>Batchmates</title>
+    <title></title>
   </head>
-        <body class=" menu-affix">
+        <body class="menu-affix">
             <div class="bg-dark dk" id="wrap">
                 <div id="top">
                     <?php include ('top_navbar.php');?>
                 </div>
                 <!-- /#top -->
-                 <?php  
+                    <?php  
                     if ($login_level == '1')
                     {
-                        include('sidebar_student.php');
+                      include('sidebar_student.php');
                     }
                     if ($login_level == '2')
                     {
-                        include('sidebar_teacher.php');
+                      include('sidebar_teacher.php');
                     }
-                    elseif ($login_level == '3')
+                    elseif($login_level == '3')
                     {
-                        include('sidebar_admin.php');
+                      include('sidebar_admin.php');
                     }
                     else
-                    {
-                    }
-                    ?>    
-                    <!-- /#left -->
+                    {}
+                    ?>                    
+                      <!-- /#left -->
                 <div id="content">
                     <div class="outer">
                         <header class="head">
                             <div class="main-bar">
                             <ol class="breadcrumb">
                               <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-                              <li class="breadcrumb-item active"> Forum</li>
+                              <li class="breadcrumb-item active"> Alumni</li>
                             </ol>
                             </div>
                             <!-- /.main-bar -->
                         </header>
                         <div class="inner bg-light lter">
-                           <?php 
+                           <div class="box">
+                             <header> <?php 
 
                             $query = mysqli_query($con,"SELECT * FROM `user_student_detail` WHERE `student_ID` = $login_id");
                             $res = mysqli_fetch_array($query);
@@ -81,57 +78,146 @@ else
                             $req_course = $res['student_department'];
                             $req_year = $yr_res['year'];
                            ?>
-                           <div class="col-sm-12">
-                                <h2 class="text-center">
-                                <?php 
-                                if ($req_course == 'IT') {
-                                    echo "Information Technology";
-                                }
-                                if ($req_course == 'COMSCI') {
-                                    echo "Computer Science";
-                                }
-                                if ($req_course == 'OA') {
-                                    echo "Office Addministration";
-                                }
-                                ?>
-                                </h2>
-                               <h1 class="text-center">Batch of <?php 
-                                echo $yr_res['year'];
-                                 ?></h1>
-                               <hr>
-                               <br><br>
-                               <table class="table table-bordered table-advance table-hover ">
-                                   <thead>
-                                       <th>Names</th>
-                                       <th>Student Number</th>
-                                   </thead>
-                                   <tbody>
-                                   <?php 
+                           
+                            <hr>
+                             </header>
+                             <div class="col-sm-12">
+                             <br>
+<div class="btn-group">
+  <button type="button" class="btn btn-primary">Course</button>
+  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" role="menu"> 
+    <li><a id="xIT">BSIT</a></li>
+    <li><a id="xCS">BSCS</a></li>
+    <li><a id="xOA">BSOA</a></li>
+  </ul>
+</div>
+<?php 
+          if ($req_course == 'IT') {
+                $zit = "show";
+                $zcs = "hidden";
+                $zoa = "hidden";
+           }
+          if ($req_course == 'COMSCI') {
+                $zit = "hidden";
+                $zcs = "show";
+                $zoa = "hidden";
+           }
+          if ($req_course == 'OA') {
+              $zit = "hidden";
+              $zcs = "hidden";
+              $zoa = "show";
+           }
+          ?>
 
-                                   $query = mysqli_query($con,"SELECT * FROM `user_student_detail`  WHERE student_department LIKE '$req_course' AND student_year_grad LIKE '$req_year%' ORDER BY `student_fName`  ASC");
 
-                                    $no = 1;
-                                  while ($data = mysqli_fetch_array($query)) {
-                                    $data['student_fName']
-                                  
-                                   ?>
-                                       <tr>
-                                           <td><?php echo $no ?>. <?php echo   $data['student_fName']." ".$data['student_mName']." ".  $data['student_lName']?></td>
-                                           <td><?php echo $data['student_IDNumber']?></td>
-                                       </tr>
-                                       <?php 
-                                       $no = $no + 1;
-                                       }
-                                       ?>
-                                   </tbody>
-                                   <tfoot>
-                                       <tr>
-                                       <td></td>
-                                       <td></td>
-                                       </tr>
-                                   </tfoot>
-                               </table>
-                           </div>
+<div id="IT_con" <?php echo $zit?>="">
+  <center><h3>Information Technology Batch of <?php echo $req_year; ?> </h3></center>
+  <table class="table table-bordered table-advance table-hover  ">
+    <thead>
+             <th>Names</th>
+             <th>Student Number</th>
+    </thead>
+    <tbody>
+      <?php 
+
+       $query = mysqli_query($con,"SELECT * FROM `user_student_detail`  WHERE student_department LIKE 'IT' AND student_year_grad LIKE '$req_year%' ORDER BY `student_fName`  ASC");
+
+        $no = 1;
+      while ($data = mysqli_fetch_array($query)) {
+        $data['student_fName']
+      
+       ?>
+           <tr>
+               <td><?php echo $no ?>. <?php echo   $data['student_fName']." ".$data['student_mName']." ".  $data['student_lName']?></td>
+               <td><?php echo $data['student_IDNumber']?></td>
+           </tr>
+           <?php 
+           $no = $no + 1;
+           }
+      ?>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th></th>
+        <th></th>
+      </tr>
+    </tfoot>
+  </table>
+</div>
+<div id="CS_con" <?php echo $zcs?>="">
+  <center><h3>Computer Science Batch of <?php echo $req_year; ?></h3></center>
+  <table class="table table-bordered table-advance table-hover  ">
+    <thead>
+             <th>Names</th>
+             <th>Student Number</th>
+    </thead>
+    <tbody>
+       <?php 
+
+       $query = mysqli_query($con,"SELECT * FROM `user_student_detail`  WHERE student_department LIKE 'COMSCI' AND student_year_grad LIKE '$req_year%' ORDER BY `student_fName`  ASC");
+
+        $no = 1;
+      while ($data = mysqli_fetch_array($query)) {
+        $data['student_fName']
+      
+       ?>
+           <tr>
+               <td><?php echo $no ?>. <?php echo   $data['student_fName']." ".$data['student_mName']." ".  $data['student_lName']?></td>
+               <td><?php echo $data['student_IDNumber']?></td>
+           </tr>
+           <?php 
+           $no = $no + 1;
+           }
+      ?>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th></th>
+        <th></th>
+      </tr>
+    </tfoot>
+  </table>
+</div>
+<div id="OA_con" <?php echo $zoa?>="">
+  <center><h3>Office Administration Batch of <?php echo $req_year; ?></h3></center>
+  <table class="table table-bordered table-advance table-hover  ">
+    <thead>
+             <th>Names</th>
+             <th>Student Number</th>
+    </thead>
+    <tbody>
+      <?php 
+
+       $query = mysqli_query($con,"SELECT * FROM `user_student_detail`  WHERE student_department LIKE 'OA' AND student_year_grad LIKE '$req_year%' ORDER BY `student_fName`  ASC");
+
+        $no = 1;
+      while ($data = mysqli_fetch_array($query)) {
+        $data['student_fName']
+      
+       ?>
+           <tr>
+               <td><?php echo $no ?>. <?php echo   $data['student_fName']." ".$data['student_mName']." ".  $data['student_lName']?></td>
+               <td><?php echo $data['student_IDNumber']?></td>
+           </tr>
+           <?php 
+           $no = $no + 1;
+           }
+      ?>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th></th>
+        <th></th>
+      </tr>
+    </tfoot>
+  </table>
+</div>
+                              
+                            </div>
+                         
                         </div>
                         <!-- /.inner -->
                     </div>
@@ -139,61 +225,6 @@ else
                 </div>
                 <!-- /#content -->
 
-                    <div id="right" class="onoffcanvas is-right is-fixed bg-light" aria-expanded=false>
-                        <a class="onoffcanvas-toggler" href="#right" data-toggle=onoffcanvas aria-expanded=false></a>
-                        <br>
-                        <br>
-                        <div class="well well-small dark">
-                            <ul class="list-unstyled">
-                                <li>Visitor <span class="inlinesparkline pull-right">1,4,4,7,5,9,10</span></li>
-                                <li>Online Visitor <span class="dynamicsparkline pull-right">Loading..</span></li>
-                                <li>Popularity <span class="dynamicbar pull-right">Loading..</span></li>
-                                <li>New Users <span class="inlinebar pull-right">1,3,4,5,3,5</span></li>
-                            </ul>
-                        </div>
-                        <!-- /.well well-small -->
-                        <!-- .well well-small -->
-                        <div class="well well-small dark">
-                            <button class="btn btn-block">Default</button>
-                            <button class="btn btn-primary btn-block">Primary</button>
-                            <button class="btn btn-info btn-block">Info</button>
-                            <button class="btn btn-success btn-block">Success</button>
-                            <button class="btn btn-danger btn-block">Danger</button>
-                            <button class="btn btn-warning btn-block">Warning</button>
-                            <button class="btn btn-inverse btn-block">Inverse</button>
-                            <button class="btn btn-metis-1 btn-block">btn-metis-1</button>
-                            <button class="btn btn-metis-2 btn-block">btn-metis-2</button>
-                            <button class="btn btn-metis-3 btn-block">btn-metis-3</button>
-                            <button class="btn btn-metis-4 btn-block">btn-metis-4</button>
-                            <button class="btn btn-metis-5 btn-block">btn-metis-5</button>
-                            <button class="btn btn-metis-6 btn-block">btn-metis-6</button>
-                        </div>
-                        <!-- /.well well-small -->
-                        <!-- .well well-small -->
-                        <div class="well well-small dark">
-                            <span>Default</span><span class="pull-right"><small>20%</small></span>
-                        
-                            <div class="progress xs">
-                                <div class="progress-bar progress-bar-info" style="width: 20%"></div>
-                            </div>
-                            <span>Success</span><span class="pull-right"><small>40%</small></span>
-                        
-                            <div class="progress xs">
-                                <div class="progress-bar progress-bar-success" style="width: 40%"></div>
-                            </div>
-                            <span>warning</span><span class="pull-right"><small>60%</small></span>
-                        
-                            <div class="progress xs">
-                                <div class="progress-bar progress-bar-warning" style="width: 60%"></div>
-                            </div>
-                            <span>Danger</span><span class="pull-right"><small>80%</small></span>
-                        
-                            <div class="progress xs">
-                                <div class="progress-bar progress-bar-danger" style="width: 80%"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /#right -->
             </div>
 
             <!-- /#wrap -->
@@ -201,5 +232,64 @@ else
             <!-- /#footer -->
             <?php include ('script.php');?>
         </body>
+         
+        <script type="text/javascript">
+
+        $(document).ready(function(){
+                                $("#xIT").click(function(){
+                                    $("#IT_con").show();
+                                    $("#CS_con").hide();
+                                    $("#OA_con").hide();
+                                });
+                                $("#xCS").click(function(){
+                                    $("#IT_con").hide();
+                                    $("#CS_con").show();
+                                    $("#OA_con").hide();
+                                });
+                                $("#xOA").click(function(){
+                                     $("#IT_con").hide();
+                                    $("#CS_con").hide();
+                                    $("#OA_con").show();
+                                });
+                            });
+
+        $(document).ready(function() {
+            console.log("document ready!");
+
+            var $sticky = $('.sticky');
+            var $stickyrStopper = $('.sticky-stopper');
+            if (!!$sticky.offset()) { // make sure ".sticky" element exists
+
+                var generalSidebarHeight = $sticky.innerHeight();
+                var stickyTop = $sticky.offset().top;
+                var stickOffset = 0;
+                var stickyStopperPosition = $stickyrStopper.offset().top;
+                var stopPoint = stickyStopperPosition - generalSidebarHeight - stickOffset;
+                var diff = stopPoint + stickOffset;
+
+                $(window).scroll(function() { // scroll event
+                    var windowTop = $(window).scrollTop(); // returns number
+
+                    if (stopPoint < windowTop) {
+                        $sticky.css({
+                            position: 'absolute',
+                            top: diff
+                        });
+                    } else if (stickyTop < windowTop + stickOffset) {
+                        $sticky.css({
+                            position: 'fixed',
+                            top: stickOffset
+                        });
+                    } else {
+                        $sticky.css({
+                            position: 'absolute',
+                            top: 'initial'
+                        });
+                    }
+                });
+
+            }
+        });
+        </script>
 
 </html>
