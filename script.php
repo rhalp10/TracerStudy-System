f6b87c25239cf894612c6dd757c351af26587d2b
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2018-01-25 20:48:14 +0800

    update and move other script

--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <!--jQuery -->
 <script src="assets/lib/jquery/jquery.js"></script>
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script>
+<script src="assets/js/prism.min.js"></script>
 
 <!--Bootstrap -->
 <script src="assets/lib/bootstrap/js/bootstrap.js"></script>
@@ -26,317 +26,44 @@
 <script src="assets/js/dataTables.bootstrap.min.js"></script>
 
 <script type="text/javascript">
-
-
  $(document).ready(function() {
-        var dataTable = $('#forumData_Unpin').DataTable( {
-          "processing": true,
-          "serverSide": true,
-          "bLengthChange": false,
-          "ordering": true,
-          "columnDefs": [ {
-                        className: "forum-td",
-                          "targets": 0,
-                          "searchable": false
-                        }],
-          "ajax":{
-            url :"serverside_data_forumUnpin.php", // json datasource
-            type: "post",  // method  , by default get
-            error: function(){  // error handling
-              $(".forumData_Unpin-error").html("");
-              $("#forumData_Unpin").append('<tbody class="forumData_Unpin-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-              $("#forumData_Unpin_processing").css("display","none");
-              
-            }
-          }
-        } );
-      } );
- $(document).ready(function() {
-                var dataTable = $('#forumData_Pin').DataTable( {
-                    // "stripeClasses": [],
-                    "processing": true,
-                    "serverSide": true,
-                    "bAutoWidth": false,
-                    
-                    "bLengthChange": false,
+        console.log("document ready!");
+       
+        var $sticky = $('.sticky');
+        var $stickyrStopper = $('.sticky-stopper');
+        if (!!$sticky.offset()) { // make sure ".sticky" element exists
 
-                    "columnDefs": [ {
-                        className: "forum-td",
-                          "targets": 0,
-                          "searchable": false
-                        }],
-                        // "initComplete": function () {
-                        //     $( document ).on("click", "tr[role='row']", function(){
-                        //         // var year_data = document.getElementById("year_data" ).innerHTML;
-                        //          var year_data = $(this).parents('div .col-sm-6 forum-list-content').data('id');
-                        //          jQuery("tr").addClass("myClass");
-                        //          window.location='alumni_view.php?course=IT&year='+year_data;
+            var generalSidebarHeight = $sticky.innerHeight();
+            var stickyTop = $sticky.offset().top;
+            var stickOffset = 0;
+            var stickyStopperPosition = $stickyrStopper.offset().top;
+            var stopPoint = stickyStopperPosition - generalSidebarHeight - stickOffset;
+            var diff = stopPoint + stickOffset;
 
-                        //     });
-                        // },
-                    "ajax":{
-                        url :"serverside_data_forumPin.php", // json datasource
-                        type: "post",  // method  , by default get
-                        error: function(){  // error handling
-                            $(".forumData_Pin-error").html("");
-                            $("#forumData_Pin").append('<tbody class="forumData_Pin-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-                            $("#forumData_Pin_processing").css("display","none");
-                            
+            $(window).scroll(function() { // scroll event
+                var windowTop = $(window).scrollTop(); // returns number
 
-                        }
-                        
-                    }
-                } );
-                
-            } );
- $(document).ready(function() {
-                var dataTable = $('#alumniIT').DataTable( {
-                    // "stripeClasses": [],
-                    "processing": true,
-                    "serverSide": true,
-                    "bAutoWidth": false,
-                    // "bSort": false,
-                    "bLengthChange": false,
+                if (stopPoint < windowTop) {
+                    $sticky.css({
+                        position: 'absolute',
+                        top: diff
+                    });
+                } else if (stickyTop < windowTop + stickOffset) {
+                    $sticky.css({
+                        position: 'fixed',
+                        top: stickOffset
+                    });
+                } else {
+                    $sticky.css({
+                        position: 'absolute',
+                        top: 'initial'
+                    });
+                }
+            });
 
-                    "info":     false,
-                    "columnDefs": [ {
+        }
+    });
 
-                        "orderable": false,
-                        className: "forum-td",
-                          "targets": 0,
-                          "searchable": false
-
-                        }],
-                        
-                        // "initComplete": function () {
-                        //     $( document ).on("click", "tr[role='row']", function(){
-                        //         // var year_data = document.getElementById("year_data" ).innerHTML;
-                        //          var year_data = $(this).parents('div .col-sm-6 forum-list-content').data('id');
-                        //          jQuery("tr").addClass("myClass");
-                        //          window.location='alumni_view.php?course=IT&year='+year_data;
-
-                        //     });
-                        // },
-
-                    "ajax":{
-                        url :"serverside_data_IT_Alumni.php", // json datasource
-                        type: "post",  // method  , by default get
-                        error: function(){  // error handling
-                            $(".alumniIT-error").html("");
-                            $("#alumniIT").append('<tbody class="alumniIT-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-                            $("#alumniIT_processing").css("display","none");
-                            
-
-                        }
-                        
-                    }
-                } );
-
-
-
-                
-            } );
- $(document).ready(function() {
-                var dataTable = $('#alumniCS').DataTable( {
-                    // "stripeClasses": [],
-                    "processing": true,
-                    "serverSide": true,
-                    "bAutoWidth": false,
-                    // "bSort": false,
-                    "bLengthChange": false,
-
-                    "info":     false,
-                    "columnDefs": [ {
-
-                        "orderable": false,
-                        className: "forum-td",
-                          "targets": 0,
-                          "searchable": false
-
-                        }],
-                        
-                        // "initComplete": function () {
-                        //     $( document ).on("click", "tr[role='row']", function(){
-                        //         // var year_data = document.getElementById("year_data" ).innerHTML;
-                        //          var year_data = $(this).parents('div .col-sm-6 forum-list-content').data('id');
-                        //          jQuery("tr").addClass("myClass");
-                        //          window.location='alumni_view.php?course=IT&year='+year_data;
-
-                        //     });
-                        // },
-
-                    "ajax":{
-                        url :"serverside_data_CS_Alumni.php", // json datasource
-                        type: "post",  // method  , by default get
-                        error: function(){  // error handling
-                            $(".alumniIT-error").html("");
-                            $("#alumniIT").append('<tbody class="alumniIT-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-                            $("#alumniIT_processing").css("display","none");
-                            
-
-                        }
-                        
-                    }
-                } );
-
-
-
-                
-            } );
-
-
-
- $(document).ready(function() {
-                var dataTable = $('#alumniOA').DataTable( {
-                    // "stripeClasses": [],
-                    "processing": true,
-                    "serverSide": true,
-                    "bAutoWidth": false,
-                    // "bSort": false,
-                    "bLengthChange": false,
-
-                    "info":     false,
-                    "columnDefs": [ {
-
-                        "orderable": false,
-                        className: "forum-td",
-                          "targets": 0,
-                          "searchable": false
-
-                        }],
-                        
-                        // "initComplete": function () {
-                        //     $( document ).on("click", "tr[role='row']", function(){
-                        //         // var year_data = document.getElementById("year_data" ).innerHTML;
-                        //          var year_data = $(this).parents('div .col-sm-6 forum-list-content').data('id');
-                        //          jQuery("tr").addClass("myClass");
-                        //          window.location='alumni_view.php?course=IT&year='+year_data;
-
-                        //     });
-                        // },
-
-                    "ajax":{
-                        url :"serverside_data_OA_Alumni.php", // json datasource
-                        type: "post",  // method  , by default get
-                        error: function(){  // error handling
-                            $(".alumniIT-error").html("");
-                            $("#alumniIT").append('<tbody class="alumniIT-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-                            $("#alumniIT_processing").css("display","none");
-                            
-
-                        }
-                        
-                    }
-                } );
-
-
-
-                
-            } );
- 
-    $(document).ready(function() {
-                var dataTable = $('#registerstud_serverside').DataTable( {
-
-                    "processing": true,
-                    "serverSide": true,
-                    "bAutoWidth": false,
-                     // "bSort": false,
-                     "bLengthChange": false,
-                     "columnDefs": [ {
-                        className: "text-center",
-                          "targets": 1,
-                          "searchable": false
-                        }, 
-                        {
-                        className: "text-center",
-                          "targets": 2,
-                          "searchable": false
-                        }, 
-                        {
-                        className: "text-center",
-                          "targets": 3,
-                          "searchable": false
-                        }, 
-                        {
-                        className: "text-center",
-                              "targets": 4,
-                              "searchable": false
-                        }],
-                    "ajax":{
-                        url :"serverside_data_registerstud.php", // json datasource
-                        type: "post",  // method  , by default get
-                        error: function(){  // error handling
-                            $(".registerstud_serverside-error").html("");
-                            $("#eregisterstud_serverside").append('<tbody class="registerstud_serverside-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-                            $("#registerstud_serverside_processing").css("display","none");
-                            
-
-                        }
-                        
-                    }
-                } );
-
-
-                
-            } );
-$(document).ready(function() {
-                var dataTable = $('#forumData_User_Unpin').DataTable( {
-                    // "stripeClasses": [],
-                    "processing": true,
-                    "serverSide": true,
-                    "bAutoWidth": false,
-                    
-                    "bLengthChange": false,
-
-                    "columnDefs": [ {
-                        className: "forum-td",
-                          "targets": 0,
-                          "searchable": false
-                        }],
-                    "ajax":{
-                        url :"serverside_data_forumData_User_Unpin.php", // json datasource
-                        type: "post",  // method  , by default get
-                        error: function(){  // error handling
-                            $(".forumData_User_Unpin-error").html("");
-                            $("#forumData_User_Unpin").append('<tbody class="forumData_User_Unpin-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-                            $("#forumData_User_Unpin_processing").css("display","none");
-                            
-
-                        }
-                        
-                    }
-                } );
-                
-            } );
- $(document).ready(function() {
-                var dataTable = $('#forumData_User_Pin').DataTable( {
-                    // "stripeClasses": [],
-                    "processing": true,
-                    "serverSide": true,
-                    "bAutoWidth": false,
-                    
-                    "bLengthChange": false,
-
-                    "columnDefs": [ {
-                        className: "forum-td",
-                          "targets": 0,
-                          "searchable": false
-                        }],
-                    "ajax":{
-                        url :"serverside_data_forumData_User_Pin.php", // json datasource
-                        type: "post",  // method  , by default get
-                        error: function(){  // error handling
-                            $(".forumData_User_Pin-error").html("");
-                            $("#forumData_User_Pin").append('<tbody class="forumData_User_Pin-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-                            $("#forumData_User_Pin_processing").css("display","none");
-                            
-
-                        }
-                        
-                    }
-                } );
-              
-            } );
 
  $(document).ready(function() {
     $('#example').DataTable( {
