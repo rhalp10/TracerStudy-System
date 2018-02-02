6afdfc7470830a090bebec98b6e140be2bd829a5
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2018-02-02 22:38:40 +0800

    add recordteacher delete

---
+++ action/recordteacher_delete_action.php
@@ -0,0 +1,23 @@
+<?php 
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$con = mysqli_connect('localhost','root','','tracerdata') or die("ERROR");
+$studentID = $_REQUEST['studentID'];
+$verfy_sql = mysqli_query($con,"SELECT * FROM `user_student_detail` WHERE student_ID = '$studentID'");
+$verfy_qry = mysqli_fetch_array($verfy_sql);
+$student_userID = $verfy_qry['student_userID'];
+if ($student_userID == 0) {
+	mysqli_query($con,"DELETE FROM `user_student_detail` WHERE `student_ID` = '$studentID'");
+}
+else
+{
+	mysqli_query($con,"DELETE FROM `user_student_detail` WHERE `student_ID` = '$studentID'");
+	mysqli_query($con,"DELETE FROM `user_account` WHERE `user_ID` = '$student_userID'");
+}
+
+
+echo "<script>alert('Successfully Deleted');
+						window.location='../recordstudent.php';
+					</script>";
+
+
+?>
